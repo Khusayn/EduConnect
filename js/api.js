3636c422b58091abbf1063bd4efeccf0066b0fce
Khusayn <husaynabdunabiyev@gmail.com>
2026-03-13 19:31:35 +0500

    fix api url

--- js/api.js
+++ js/api.js
@@ -1,7 +1,5 @@
 // ===== API SERVICE =====
-const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-  ? 'http://localhost:5000/api'
-  : '/api';
+const API_URL = 'https://educonnectstudy.onrender.com/api';
 
 const Auth = {
   getToken: () => localStorage.getItem('edu_token'),
